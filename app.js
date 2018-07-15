03bafb7997f14530c83daf0dcc1df5dc9637306d
doctafaustus <clowd150@gmail.com>
2018-07-14 20:43:55 -0500

    Config public directory for prod

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
-
+app.use(express.static(`${__dirname}/grubster/dist/static`));
 app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
 app.use(session({
   secret: 'keyboard cat',
