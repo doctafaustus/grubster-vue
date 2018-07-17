3933c125dbe9a252e6aa0c7b5541db029b8c80f9
doctafaustus <clowd150@gmail.com>
2018-07-16 23:22:35 -0500

    Update auth0 config URLs

--- grubster/src/auth/auth0-variables.js
+++ grubster/src/auth/auth0-variables.js
@@ -1,3 +1,5 @@
+window.endpoint = (window.location.origin.indexOf('http://localhost') > -1) ? 'http://localhost:3000' : 'https://grubster-app.herokuapp.com';
+
 export const AUTH_CONFIG = {
   clientId: 'IZzdmaJh7q7CdHM07VVSJzdXi9Dbp7Tp',
   domain: 'grubster.auth0.com',
