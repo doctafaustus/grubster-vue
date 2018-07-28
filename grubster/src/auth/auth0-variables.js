318ba481bfe607db4ef8c6f26784637a90958b6b
doctafaustus <clowd150@gmail.com>
2018-07-28 13:23:48 -0500

    Update universal endpoint to use www

--- grubster/src/auth/auth0-variables.js
+++ grubster/src/auth/auth0-variables.js
@@ -1,4 +1,4 @@
-window.endpoint = (window.location.origin.indexOf('http://localhost') > -1) ? 'http://localhost:8080' : 'http://grubster.me';
+window.endpoint = (window.location.origin.indexOf('http://localhost') > -1) ? 'http://localhost:8080' : 'http://www.grubster.me';
 
 export const AUTH_CONFIG = {
   clientId: 'IZzdmaJh7q7CdHM07VVSJzdXi9Dbp7Tp',
